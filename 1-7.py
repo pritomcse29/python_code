c41c8126060331d021830274eeaecdd79fdbc72d
Pritom Sarkar <pritom.cse.29@gmail.com>
2025-02-02 17:56:32 +0600

    while loop , comparison operator

---
+++ 1-7.py
@@ -0,0 +1,8 @@
+# 1.7 while loop in python
+# break
+# continue
+
+num = 1
+while num <= 10:
+    print(num)
+    num = num +1 
--- comparison.py
+++ comparison.py
@@ -13,5 +13,37 @@ num1 = 5
 power = num1 ** num1
 print(power)
 
+"""
+Python Arithmetic Operators
+Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+Operator	Name	Example	Try it
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+
+
+https://www.w3schools.com/python/python_operators.asp
+
+"""
+# += , -+ ,*= ,/=
+
+# in,not, not in , is , is not 
+a = 7
+if a > 5:
+    print('5 er besi')
+    print('koto besi ke jane')
+else:
+    print('choto choto rate')
+
+# if boss == True
+# if boss is True
+
+#and or
+
 
 
